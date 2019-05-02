e88053714a5610eb7f08e548e2bcb28fa7dbb4ce
Daniel Castellarin <daniel.r.castellarin@gmail.com>
2019-05-02 10:34:10 -0400

    Started creating Page virtual class.

---
+++ src/home/Page.java
@@ -0,0 +1,32 @@
+package home;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+
+public class Page {
+
+    private Scene scene;
+    BorderPane pane;
+    HBox header;
+
+    public void createHeader(String headerColor, String headerText, Paint textColor){
+        header = new HBox();
+        header.setPadding(new Insets(30));
+        header.setStyle(headerColor);
+        Text title = new Text(headerText);
+        title.setFill(textColor;
+        title.setFont(Font.font("Constantia", FontWeight.SEMI_BOLD, 36.0));
+        header.getChildren().add(title);
+        header.setAlignment(Pos.CENTER);
+        pane.setTop(header);
+    }
+
+}
