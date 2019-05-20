300c864f8180b9f10d5b542f4c0b52c523ff94da
Daniel Castellarin <daniel.r.castellarin@gmail.com>
2019-05-20 13:48:08 -0400

    Slider functionality works using arrow keys in pop up. Conversion function works too

--- src/home/StudentTimePopUp.java
+++ src/home/StudentTimePopUp.java
@@ -1,13 +1,19 @@
 package home;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class StudentTimePopUp extends Page{
@@ -17,6 +23,8 @@ public class StudentTimePopUp extends Page{
     EventHandler<ActionEvent> cancelEvent;
     EventHandler<ActionEvent> doneEvent;
     EventHandler<ActionEvent> addTimeEvent;
+    int startSliderVal;
+    int endSliderVal;
 
     public StudentTimePopUp(){
         popUpWindow = createStage();
@@ -38,7 +46,81 @@ public class StudentTimePopUp extends Page{
     }
 
     private void createTimeSelector(){
+        HBox hbox = new HBox();
 
+        //create layout here
+
+        Text dayLabel = new Text("Day of the Week:");
+
+        ComboBox dayChooser = new ComboBox();
+        dayChooser.getItems().addAll("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+        Text startValueLabel = new Text();
+
+        Slider startTime = new Slider();
+        startTime.setMin(0);
+        startTime.setMax(2400);
+        startTime.setValue(1200);
+        startTime.setShowTickMarks(true);
+        startTime.setBlockIncrement(25);
+        startTime.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                startValueLabel.setText(numToTimeConvert(t1.intValue()));
+                System.out.println("OG val: " + t1);
+                System.out.println("Time: " + startValueLabel.getText());
+            }
+        });
+
+        Slider endTime = new Slider();
+
+
+        hbox.getChildren().add(dayChooser);
+        hbox.getChildren().add(startTime);
+        hbox.setAlignment(Pos.CENTER);
+        pane.setCenter(hbox);
+//        pane.setCenter();
+    }
+
+    /*
+    Each interval is 15 minutes
+    Slider increments by 25
+    minute conversion --> og * 3/5
+
+    divide by hundred to get rid of last 2 digits
+    analyze to determine morning/evening
+
+     */
+    private String numToTimeConvert(int num){
+        String time;
+        boolean isEvening = false;
+
+
+        int hours = num / 100;
+        int minutes = (num - (hours * 100)) * 3/5;
+
+        if(hours > 12){
+            hours -= 12;
+            isEvening = true;
+        }else if (hours == 0){
+            hours = 12;
+        }
+
+        if(minutes == 0){
+            if (isEvening){
+                time = hours + ":" + minutes + "0pm";
+            }else{
+                time = hours + ":" + minutes + "0am";
+            }
+        }else{
+            if (isEvening){
+                time = hours + ":" + minutes + "pm";
+            }else{
+                time = hours + ":" + minutes + "am";
+            }
+        }
+
+        return time;
     }
 
     private void createButtonEvents(){
