b96c60f396306a0272fd39c45531c6ffdb499c1f
Daniel Castellarin <daniel.r.castellarin@gmail.com>
2019-06-13 14:18:45 -0400

    Rough draft of annotations created for Main and HomePage

--- src/home/HomePage.java
+++ src/home/HomePage.java
@@ -7,20 +7,35 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
+/**
+ *
+ */
 public class HomePage extends Page {
 
-    EventHandler<ActionEvent> goStudent;
-    EventHandler<ActionEvent> goTutor;
+    private EventHandler<ActionEvent> goStudent;
+    private EventHandler<ActionEvent> goTutor;
 
     public static StudentPage studentPage;
     public static TutorLogin tutorLogin;
 
+
+    /**
+     * This is the constructor for HomePage, which is called only once from Main.
+     * HomePage extends Page, which is the base class for all Pages in this program.
+     * Generic scene setup is done in Page.
+     *
+     * *** I highly suggest going to Page to learn the setup for each page ***
+     *
+     * HomePage is unique in that its navigation buttons are in the center of the Page,
+     * so its initialization is quite simple.
+     *
+     * @param stage the stage that is used for the rest of the program
+     *              and is passed along to each new page.
+     * @see Page
+     */
     public HomePage(Stage stage){
-        stage.setTitle("Peer Tutoring Portal");
         createHeader("-fx-background-color: firebrick", "Peer Tutoring Portal", Color.FLORALWHITE);
         createButtonEvents(stage);
         createCenter();
@@ -29,6 +44,16 @@ public class HomePage extends Page {
         stage.show();
     }
 
+    /**
+     * This is where the events for each button on HomePage is created.
+     * goStudent creates the StudentPage when the Student button is pressed.
+     * goTutor creates the TutorLogin page when the Tutor button is pressed.
+     * These buttons are both created in createCenter.
+     *
+     * @param stage used to pass on the stage to the next page
+     *              This is so that the same stage (window) can be used by each page
+     *              instead of having to create new stages for each page.
+     */
     private void createButtonEvents(Stage stage){
         goStudent = new EventHandler<ActionEvent>() {
             @Override
@@ -44,6 +69,15 @@ public class HomePage extends Page {
         };
     }
 
+    /**
+     * createCenter sets up the center of this page
+     * VBox is a type of layout, where all of its nodes are arranged in a vertical column.
+     * Nodes are features on the page. Examples are text, buttons, hyperlinks, etc.
+     * The color, positioning, and spacing of nodes is set at the beginning.
+     * Both buttons are created through methods defined in Page.
+     * centerBox then adds the buttons to its list of nodes.
+     * centerBox then set to the center of the page.
+     */
     private void createCenter(){
         VBox centerBox = new VBox();
         centerBox.setStyle("-fx-background-color: silver");
--- src/home/Main.java
+++ src/home/Main.java
@@ -7,13 +7,26 @@ public class Main extends Application {
 
     public static HomePage homePage;
 
+    /**
+     * First code that is run in the program. "Launches" the program
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Inside start, the stage is given a name.
+     *
+     * A stage is a wiindow. Examples of windows are Chrome, Microsoft Word, etc.
+     *
+     * This is also where the home page is created.
+     *
+     * @param primaryStage this is the stage that is used for the rest of the program
+     *                     and is passed along to each page that is created.
+     */
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Peer Tutoring Portal");
         homePage = new HomePage(primaryStage);
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
